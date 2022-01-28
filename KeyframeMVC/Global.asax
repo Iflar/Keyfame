7a266800b786f14065a2253bc8f80bad8a3f8fe3
iflar <classicsience@gmail.com>
2022-01-27 20:34:50 -0500

    Add project files.

---
+++ KeyframeMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KeyframeMVC.MvcApplication" Language="C#" %>
---
+++ KeyframeMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KeyframeMVC.Startup))]
+namespace KeyframeMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

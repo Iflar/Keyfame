5267dcb78339f76984d1ec118af65314bb0ed0ce
iflar <classicsience@gmail.com>
2022-02-01 21:28:58 -0500

    working on user roles

--- Keyframe.Data/UserProfile.cs
+++ Keyframe.Data/UserProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace Keyframe.Data
 {
     public class UserProfile
     {
+        [Key]
+        public int UserId { get; set; }
+        public Guid OwnerId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Biography { get; set; }
         public string ProfilePictureURL { get; set; }
 
